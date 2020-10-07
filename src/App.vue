28fdf0ca475af302bf90fc7c202d3d167ff5aaea
mdajgarhossain <mdajgarhossain1@gmail.com>
2020-10-07 13:55:57 +0600

    Registar component globally & nesting it inside App

--- src/App.vue
+++ src/App.vue
@@ -2,28 +2,25 @@
   <div id="app">
     <!-- this h1 is App component's own data -->
     <h1>{{ title }}</h1>
-    <!-- pass props by binding data -->
-    <HelloWorld :msg="msg" />
-    <!-- pass props by providing direct value-->
-    <HelloWorld msg="Direct messgae from App" />
-    <!-- no props data passing & using default props-->
-    <HelloWorld />
+    <!-- methods in component -->
+    <h2>{{ greeting() }}</h2>
+    <!-- Using globally registered component -->
+    <topics></topics>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
-  name: "App",
-  components: {
-    HelloWorld,
-  },
   data() {
     return {
       title: "I am App component",
       msg: "Message from App",
     };
+  },
+  methods: {
+    greeting() {
+      return "Vue is nice";
+    },
   },
 };
 </script>
---
+++ src/components/Topics.vue
@@ -0,0 +1,20 @@
+<template>
+  <ul>
+    <li v-for="(framework, index) in frameworks" v-bind:key="index">
+      {{ framework }}
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      frameworks: ["vueJS", "reactJS", "NextJS", "ExpressJS"],
+    };
+  },
+};
+</script>
+
+<style>
+</style>
