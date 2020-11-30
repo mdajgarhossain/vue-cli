5cc3828f70600bf5fde941509d963689aa203f43
mdajgarhossain <mdajgarhossain1@gmail.com>
2020-11-30 18:19:44 +0600

    Create modal with vue transition

--- src/App.vue
+++ src/App.vue
@@ -1,69 +1,21 @@
 <template>
   <div>
-    <Header :headline="headline" />
-    <Developers :developers="developers" :title="title" />
-    <hr />
-    <Developers :developers="developers" :title="title" />
-    <Footer :headline="headline" />
+    <!-- Modal -->
+    <ModalVueTransition />
   </div>
 </template>
 
 <script>
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Developers from "./components/Developers";
+import ModalVueTransition from "./components/modals/ModalVueTransition";
 
 export default {
   components: {
-    Header,
-    Footer,
-    Developers,
+    ModalVueTransition,
   },
   data() {
-    return {
-      developers: [
-        {
-          id: 1,
-          name: "Md. Zakaria Mazumder",
-          speciality: "Senior Software Developer",
-          show: false,
-        },
-        {
-          id: 2,
-          name: "Md. Saikot Mazumder",
-          speciality: "Senior Software Developer",
-          show: false,
-        },
-        {
-          id: 3,
-          name: "Sirjis Khan",
-          speciality: "Fullstack Developer",
-          show: false,
-        },
-        {
-          id: 4,
-          name: "Kanak Hasan",
-          speciality: "Front-end Developer",
-          show: false,
-        },
-        {
-          id: 5,
-          name: "Bappi Khan",
-          speciality: "Front-end Developer",
-          show: false,
-        },
-        {
-          id: 6,
-          name: "Md. Ajgar Hossain Jewel",
-          speciality: "Intern",
-          show: false,
-        },
-      ],
-      headline: "All Developers List",
-    };
+    return {};
   },
 };
 </script>
 
-<style>
-</style>
+<style></style>
